36a06b9f6f0fcebdc56913f193c52e21a6c380e7
Vasyl <khorev.41@gmail.com>
2024-07-18 22:37:06 +0200

    added Dashboard page about bachelor thesis

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://vasylkhorev.github.io',
+  site: 'https://vasylkhorev.github.io',
+  integrations: [tailwind()]
 });
-
