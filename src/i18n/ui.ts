322b3c3449652201e0daa1ae14cbdb52ef93825b
Vasyl <khorev.41@gmail.com>
2024-07-19 14:32:57 +0200

    added Languages support for EN, SK,UA created Thesis page created About page

---
+++ src/i18n/ui.ts
@@ -0,0 +1,28 @@
+export const languages = {
+    en: 'English',
+    sk: 'Slovenčina',
+    ua: "Українська",
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+    en: {
+        'nav.home': 'Home',
+        'nav.about': 'About',
+        'nav.thesis': 'Thesis',
+        "allrightsreserved": "All rights reserved",
+    },
+    sk: {
+        'nav.home': 'Domov',
+        'nav.about': 'O mne',
+        'nav.thesis': 'Bakalárska práca',
+        "allrightsreserved": "Všetky práva vyhradené",
+    },
+    ua: {
+        'nav.home': 'Головна',
+        'nav.about': 'Про мене',
+        'nav.thesis': 'Дипломна робота',
+        "allrightsreserved": "Всі права захищені",
+    },
+} as const;
---
+++ src/i18n/utils.ts
@@ -0,0 +1,19 @@
+import { ui, defaultLang } from './ui';
+
+export function getLangFromUrl(url: URL) {
+    const [, lang] = url.pathname.split('/');
+    if (lang in ui) return lang as keyof typeof ui;
+    return defaultLang;
+}
+
+export function replaceLangInUrl(url: URL, key: String) : String {
+    return `/${key}/${url.pathname.substring(4)}`
+}
+
+
+export function useTranslations(lang: keyof typeof ui) {
+    return function t(key: keyof typeof ui[typeof defaultLang]) {
+        // @ts-ignore
+        return ui[lang][key] || ui[defaultLang][key];
+    }
+}
