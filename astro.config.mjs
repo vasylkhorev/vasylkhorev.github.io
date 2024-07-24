f553544620f22e5d76386ea27c9b8a99e3c92232
Vasyl <khorev.41@gmail.com>
2024-07-24 19:23:35 +0200

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import sitemap from '@astrojs/sitemap';
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import mdx from "@astrojs/mdx";
@@ -8,7 +9,7 @@ import rehypePrettyCode from "rehype-pretty-code";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://vasylkhorev.github.io',
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), sitemap()],
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex, rehypePrettyCode],
